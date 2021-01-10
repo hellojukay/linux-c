ad2ef6e7584a449882804a3a24c7332013c012ee
hellojukay <hellojukay@163.com>
2021-01-10 06:01:16 +0000

    实现文件并发下载

--- seek/main.go
+++ seek/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"mime"
 	"net/http"
 	"os"
+	"path"
 	"runtime"
 	"sync"
 )
@@ -19,10 +23,6 @@ func init() {
 	flag.Parse()
 }
 
-type fileInfo struct {
-	size     int64
-	filename string
-}
 type Range struct {
 	Start int64
 	End   int64
@@ -37,18 +37,34 @@ type Downloader struct {
 
 // 下载文件，等待所有 goroutine 返回
 func (loader *Downloader) Down(thread int) error {
-	info, err := getFileInfo(loader.Url)
+	// 链接服务器，获取文件大小
+	res, err := http.Get(loader.Url)
 	if err != nil {
 		return err
 	}
-	fh, err := os.OpenFile(info.filename, os.O_CREATE|os.O_RDWR, 0644)
+	defer res.Body.Close()
+	if filename == "" {
+		filename = path.Base(res.Request.URL.Path)
+		_, params, err := mime.ParseMediaType(res.Header.Get("Content-Disposition"))
+
+		if err == nil {
+			if params["filename"] != "" {
+				filename = params["filename"]
+			}
+		}
+	}
+	log.Printf("download file from %s,content length %d\n", loader.Url, res.ContentLength)
+	fh, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		if err != nil {
 			return err
 		}
 	}
+	log.Printf("downloading file %s\n", filename)
+	defer fh.Close()
+	defer fh.Sync()
 	loader.fh = fh
-	ranges, err := loader.getRange()
+	ranges, err := loader.getRange(res.ContentLength)
 	if err != nil {
 		return err
 	}
@@ -66,26 +82,45 @@ func (loader *Downloader) Down(thread int) error {
 // 下载分片
 func (loader *Downloader) down(r Range) error {
 	defer loader.wg.Add(-1)
-	log.Printf("downloading content range %d to %d\n", r.Start, r.End)
-	loader.fh.Seek(r.Start, 0)
+	req, err := http.NewRequest("GET", loader.Url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", r.Start, r.End))
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("request %s error %s\n", loader.Url, err)
+		return err
+	}
+	defer res.Body.Close()
+	var buffer []byte = make([]byte, 1024*1024)
+	var off = r.Start
+	for {
+		n, err := res.Body.Read(buffer)
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		_, err = loader.fh.WriteAt(buffer[:n], off)
+		if err != nil {
+			log.Printf("write to file error %s\n", err)
+			os.Exit(1)
+		}
+		off = off + int64(n)
+	}
 	return nil
 }
 
 // 获取分配下载区间
-func (loader *Downloader) getRange() ([]Range, error) {
-	// 链接服务器，获取文件大小
-	res, err := http.Get(loader.Url)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	log.Printf("download file from %s,content length %d", loader.Url, res.ContentLength)
+func (loader *Downloader) getRange(contentLength int64) ([]Range, error) {
 	var result []Range
-	avg := res.ContentLength / int64(runtime.NumCPU())
+	avg := contentLength / int64(runtime.NumCPU())
 	var start, end int64
 	for {
-		if int64(start+avg) >= res.ContentLength {
-			end = res.ContentLength
+		if int64(start+avg) >= contentLength {
+			end = contentLength
 			result = append(result, Range{Start: start, End: end})
 			break
 		} else {
@@ -95,14 +130,6 @@ func (loader *Downloader) getRange() ([]Range, error) {
 		start = end + 1
 	}
 	return result, nil
-}
-
-func getFileInfo(url string) (*fileInfo, error) {
-	return &fileInfo{
-		size:     0,
-		filename: "a.txt",
-	}, nil
-
 }
 
 func main() {
